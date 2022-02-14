7faf55988377220db576459e0d29ba73e602744b
Ryan McCarthy <rmmcc92@aol.com>
2022-02-14 13:48:20 -0500

    built the user login route and installed jwt for authentication

--- pages/login.jsx
+++ pages/login.jsx
@@ -10,15 +10,30 @@ import React from "react";
 import Layout from "../components/Layout";
 import useStyles from "../utils/styles";
 import NextLink from "next/link";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Login() {
   const classes = useStyles();
 
-  const loginHandler = () => {};
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/api/users/login", {
+        email,
+        password,
+      });
+      console.log("login success", data);
+    } catch (err) {
+      console.log(err.response.data ? err.response.data.message : err.message);
+    }
+  };
 
   return (
     <Layout title="Login">
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
@@ -30,6 +45,7 @@ export default function Login() {
               id="email"
               label="Email"
               inputProps={{ type: "email" }}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
@@ -39,16 +55,11 @@ export default function Login() {
               id="password"
               label="Password"
               inputProps={{ type: "password" }}
+              onChange={(e) => setPassword(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
-            <Button
-              variant="contained"
-              type="submit"
-              fullWidth
-              color="primary"
-              onClick={loginHandler}
-            >
+            <Button variant="contained" type="submit" fullWidth color="primary">
               Login
             </Button>
           </ListItem>
